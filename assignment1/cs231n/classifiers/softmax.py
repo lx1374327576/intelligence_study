36fb2458a295f875e49d24c5344e36dda70e9c94
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-03-28 09:55:04 +0800

    add softmax
    loss and gradient

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,25 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  now = X.dot(W)
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  for i in range(num_train):
+    sigma_tmp = 0.0
+    yi_tmp = 0.0
+    for j in range(num_classes):
+      if j == y[i]:
+        yi_tmp = np.exp(now[i, j])
+      sigma_tmp += np.exp(now[i, j])
+    dW[:, y[i]] += -np.log(np.e) * (sigma_tmp-yi_tmp) / sigma_tmp * X[i]
+    for j in range(num_classes):
+      if j != y[i]:
+        dW[:, j] += np.log(np.e) / sigma_tmp * np.exp(now[i, j]) * X[i]
+    loss += -np.log(yi_tmp/sigma_tmp)
+  dW /= num_train
+  dW += reg * W
+  loss /= num_train
+  loss += reg*np.sum(W*W)
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +71,21 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  now = X.dot(W)
+  now_exp = np.exp(now)
+  now_sum = np.sum(now_exp, axis=1)
+  # print(now_sum)
+  arr = np.arange(X.shape[0])
+  now_tmp=(np.exp(now[arr, y[arr]]) / now_sum)
+  # print(now_tmp)
+  loss += -np.sum(np.log(now_tmp))
+  loss /= X.shape[0]
+  loss += reg*np.sum(W*W)
+  margin = now_exp / now_sum.reshape(X.shape[0], 1)
+  margin[arr, y] -= 1
+  dW = X.T.dot(margin)
+  dW /= X.shape[0]
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
